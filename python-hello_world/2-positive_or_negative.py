f3afae08beb03b3aee4dae59d1f597c0acb5190a
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-07-15 23:00:50 +0100

    fix some PEP issues

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -3,8 +3,9 @@ import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
 if number > 0:
-    print(number,'is positive')
+    print(number, 'is positive')
 elif number < 0:
     print(number, 'is negative')
 else:
     print(number, 'is zero')
+    
