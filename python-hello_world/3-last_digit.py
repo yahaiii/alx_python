1572a09102c8f39d2f0e111e0e924b8bc6c63c14
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-07-18 11:45:51 +0100

    restore own code for 3-last

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-new_number = str(number)
+# YOUR CODE HERE
+last_digit = abs(number) % 10
+if number < 0:
+    last_digit = -last_digit
 
-if number > 0 and int(new_number[-1]) != 0:
-    if int(new_number[-1]) > 5:
-        print("Last digit of {0} is {1} and is greater than 5".format(number, new_number[-1]))
-    else:
-        print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, new_number[-1]))
-elif (number < 0 and int(new_number[-1]) == 0) or (number > 0 and int(new_number[-1]) == 0):
-        print("Last digit of {0} is {1} and is 0".format(number, new_number[-1]))
-elif number == 0:
-    print("Last digit of {0} is {1} and is 0".format(number, number))
+print("Last digit of", number, "is", last_digit, end=" ")
+
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
 else:
-    print("Last digit of {0} is -{1} and is less than 6 and not 0".format(number, new_number[-1]))
+    print("and is less than 6 and not 0")
