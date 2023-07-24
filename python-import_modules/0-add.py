fa7055e068197a7c45a0d3f3385aed6b58d8b4f5
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-07-24 22:38:15 +0100

    modify: script check

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+from add_0 import add
+
 a = 1
 b = 2
 
-from add_0 import add
-
 result = add(a, b)
 
-print ("{} + {} = {}".format(a, b, result))
+print("{} + {} = {}".format(a, b, result))
 
 # Check if the script is being run directly
-if __name__ == "__main__":
-    main()
+if __name__ == "__0-add__":
+    0-add()
