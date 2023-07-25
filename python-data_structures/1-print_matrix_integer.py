0b422d3d29181a411967457a650d9b79a1b5d77a
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-07-26 00:31:14 +0100

    modify: spacing after output

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -8,9 +8,13 @@ def print_matrix_integer(matrix=[[]]):
     # Loop through each row in the matrix
     for row in matrix:
         # Loop through each element in the row
-        for element in row:
-            # Print the element with proper formatting, followed by a space
-            print("{:d}".format(element), end=" ")
+        for i, element in enumerate(row):
+            # Print the element with proper formatting
+            print("{:d}".format(element), end="")
+            
+            # Print a space unless it's the last element in the row
+            if i < len(row) - 1:
+                print(" ", end="")
         
         # Move to the next line after printing all elements in the row
         print()
