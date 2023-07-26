124de1a897c0333fdb3445dddcced5482af02dda
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-07-26 22:45:44 +0100

    modify update_dictionary

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 def update_dictionary(a_dictionary, key, value):
-    # Update the dictionary with the new key-value pair
-    a_dictionary[key] = value
+    # Create a new dictionary with the same content as a_dictionary
+    new_dict = dict(a_dictionary)
+
+    # Update the new dictionary with the new key-value pair
+    new_dict[key] = value
+
+    # Return the new dictionary
+    return new_dict
