26920398574792ec013af15ae408c72f5771dcf1
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-07-26 22:32:20 +0100

    1-common_elements

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+def common_elements(set_1, set_2):
+    # Use the intersection operation to find the common elements
+    common_elements_set = set_1.intersection(set_2)
+    return common_elements_set
