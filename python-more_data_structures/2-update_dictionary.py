876ca663852cfdc19cce4b5b5c55a9dd58a8013d
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-07-27 07:08:48 +0100

    modify: update_dictionary, check for existing key

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -3,8 +3,13 @@ def update_dictionary(a_dictionary, key, value):
     # Create a new dictionary with the same content as a_dictionary
     new_dict = dict(a_dictionary)
 
-    # Update the new dictionary with the new key-value pair
-    new_dict[key] = value
+    # Check if the key exists in the original dictionary
+    if key in a_dictionary:
+        # Update the value associated with the existing key
+        new_dict[key] = value
+    else:
+        # Add the new key-value pair to the new dictionary
+        new_dict[key] = value
 
     # Return the new dictionary
     return new_dict
