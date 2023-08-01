8f0eb7e66485bfb3d5f2cb5240a03e0ca2b23b7f
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-01 09:19:29 +0100

    add 0-square

---
+++ python-classes/0-square.py
@@ -0,0 +1,16 @@
+class Square:
+    """
+    A class that represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square object with the given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
