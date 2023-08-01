ce6bf22456aba9369ee624a4bff59ce5abd2ef8e
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-01 09:35:57 +0100

    modify: add module docstring

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,33 @@
+"""
+My Square Class
+
+This module defines a simple Square class to represent squares in a two-dimensional space.
+The Square class has an attribute 'size' to store the size of the square. The size is a positive
+integer that represents the length of each side of the square.
+
+Attributes:
+    size (int): The size of the square, i.e., the length of each side.
+
+Methods:
+    __init__(self, size): Constructor method to initialize the Square object with a given size.
+    area(self): Calculate and return the area of the square.
+    perimeter(self): Calculate and return the perimeter of the square.
+    __str__(self): Return a string representation of the Square object.
+
+Example:
+    # >>> square1 = Square(5)
+    # >>> print(square1.area())
+    25
+    # >>> print(square1.perimeter())
+    20
+    # >>> print(square1)
+    Square with size 5
+
+Note:
+    This implementation of the Square class does not perform type or value validation on the 'size' attribute.
+    It is assumed that the 'size' attribute will always be a positive integer.
+"""
+
 class Square:
     """
     A class that represents a square.
