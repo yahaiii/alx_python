8af1c6cd3fd0bed22727f95e4a5554969cbb29d6
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-03 13:30:37 +0100

    modify 2-inherits_from, edit: minify docstring

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,16 +1,13 @@
 """
 Module: 2-inherits_from
-
 A function that checks if an object is an instance of a class that inherited
 (directly or indirectly) from the specified class.
 
 """
 
+
 def inherits_from(obj, a_class):
     """
-    Check if the object is an instance of a class that inherited (directly or indirectly)
-    from the specified class.
-
     Args:
         obj: The object to check.
         a_class: The class to 
@@ -19,6 +16,5 @@ def inherits_from(obj, a_class):
     return issubclass(type(obj), a_class) and type(obj) is not a_class
     """
     Returns:
-            bool: True if the object is an instance of a class that inherited (directly or indirectly)
-            from the specified class; otherwise False.
+            bool: True if the object is an instance of a_class; otherwise False.
     """
