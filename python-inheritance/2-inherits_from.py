a117b32f729d2bde2efca81231643d9e2470ff17
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-03 13:12:58 +0100

    modify 2-inherits_from, modify: return condition

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -35,9 +35,9 @@ def inherits_from(obj, a_class):
         # >>> inherits_from(b, B)
         False
     """
-    return issubclass(type(obj), a_class)
-"""
-Returns:
-        bool: True if the object is an instance of a class that inherited (directly or indirectly)
-        from the specified class; otherwise False.
-"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    """
+    Returns:
+            bool: True if the object is an instance of a class that inherited (directly or indirectly)
+            from the specified class; otherwise False.
+    """
