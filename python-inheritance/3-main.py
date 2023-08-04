718e8aea2d39f3a8a4c2c93eb348cea07ac45f0f
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-04 09:54:35 +0100

    modify 3-main, remove test prints

--- python-inheritance/3-main.py
+++ python-inheritance/3-main.py
@@ -3,8 +3,6 @@ BaseGeometry = __import__('3-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-# print(bg)
-print("***dir(bg)***")
+print(bg)
 print(dir(bg))
-print("***dir(BaseGeometry)****")
 print(dir(BaseGeometry))
