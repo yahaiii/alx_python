49c704537a8fa7ac9cb767e39a9af75242d26a08
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-04 10:48:16 +0100

    I feel like a god rn!

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,11 +1,8 @@
-"""
-This module defines the BaseGeometry class.
-
-The BaseGeometry class is an abstract class that cannot be instantiated. It is used as a base class for other classes that will implement the area() method.
-
-The area() method is not implemented because the class BaseGeometry is an abstract class.
-"""
 class definitionOverrideMetaClass(type):
+    def __new__(cls, name, bases, attrs):
+        # Customize the class creation process here
+        return super().__new__(cls, name, bases, attrs)
+
     def __dir__(cls):
         """
         Returns:
@@ -13,9 +10,9 @@ class definitionOverrideMetaClass(type):
         """
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
-class BaseGeometry(definitionOverrideMetaClass):
+class BaseGeometry(definitionOverrideMetaClass('BaseGeometry', (), {})):
     """
-    This class defines a public instance method called area() that raises an exception with the message area() is not implemented.
+    This class defines a public instance method called area() that raises an Exception exception with the message area() is not implemented.
     """
 
     def area(self):
@@ -35,4 +32,5 @@ class BaseGeometry(definitionOverrideMetaClass):
             list: List of attributes excluding __init_subclass__.
         """
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
 
