7d49f82de5146bb696cf9a2dcf8fdf7b4122c040
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-07 14:44:36 +0100

    add 8-square

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,78 @@
+"""
+This module defines the Rectangle class.
+
+The Rectangle class inherits from the BaseGeometry class. It has two private attributes: width and height. These attributes are validated by the integer_validator() method from the BaseGeometry class.
+
+The Rectangle class has an area() method that calculates the area of the rectangle.
+"""
+
+class definitionOverrideMetaClass(type):
+    """def __new__(cls, name, bases, attrs):
+        # Customize the class creation process here
+        return super().__new__(cls, name, bases, attrs)"""
+
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+Rectangle = __import__('7-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    This class defines a rectangle.
+
+    Attributes:
+        _width (int): The width of the rectangle.
+        _height (int): The height of the rectangle.
+
+    Methods:
+        area(): Calculates the area of the rectangle.
+
+    Raises:
+        AttributeError: If the attribute width or height is accessed.
+        TypeError: If the value of the attribute width or height is not an integer.
+        ValueError: If the value of the attribute width or height is less than or equal to 0.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If the value of the attribute width or height is not an integer.
+            ValueError: If the value of the attribute width or height is less than or equal to 0.
+        """
+        self.integer_validator("size", size)
+        self._size = size
+
+    def area(self):
+        """
+        Calculates the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self._size * self._size
+    
+    def __str__(self):
+        """
+        Returns the rectangle description.
+
+        Returns:
+            str: The rectangle description in the format [Rectangle] <width>/<height>.
+        """
+        return "[Rectangle] {}/{}".format(self._size, self._size)
+
+    def __dir__(cls):
+        """
+        Returns:
+            list: List of attributes excluding __init_subclass__.
+        """
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
