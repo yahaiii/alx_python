98a09ec88ea1ff426b9ce69ae895bfafa0483c1f
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-07 12:51:36 +0100

    modify 3-base_geometry, fix the metaclass patch

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -8,16 +8,18 @@ Classes:
 """
 
 class definitionOverrideMetaClass(type):
+    def __new__(cls, name, bases, attrs):
+        # Customize the class creation process here
+        return super().__new__(cls, name, bases, attrs)
+
     def __dir__(cls):
         """
         Returns:
             list: List of attributes excluding __init_subclass__.
         """
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-      
 
-
-class BaseGeometry(metaclass=definitionOverrideMetaClass):
+class BaseGeometry(definitionOverrideMetaClass('BaseGeometry', (), {})):
     """
     This class serves as the base for other geometry-related classes.
     """
