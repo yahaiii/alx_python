323f6103154c3cd4717afcc8be6789d422a2c652
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-09 03:56:57 +0100

    modify rectangle, add area()

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -110,5 +110,8 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if self.__y < 0:
             raise ValueError("y must be >= 0")
+        
+    def area(self):
+        return self.__width * self.__height
 
 
