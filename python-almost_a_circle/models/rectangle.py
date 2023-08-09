c59dcc96f8344a2a0213a47088524ac35f18d2a0
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-09 02:38:58 +0100

    modify rectangle, add double underscore for private attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,49 +17,49 @@ class Rectangle(Base):
             id: The ID of the rectangle.
         """
         super().__init__(id)
-        self._width = width
-        self._height = height
-        self._x = x
-        self._y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
         """Get the width of the rectangle."""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """Set the width of the rectangle."""
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
         """Get the height of the rectangle."""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """Set the height of the rectangle."""
-        self._height = value
+        self.__height = value
 
     @property
     def x(self):
         """Get the x-coordinate of the rectangle."""
-        return self._x
+        return self.__x
 
     @x.setter
     def x(self, value):
         """Set the x-coordinate of the rectangle."""
-        self._x = value
+        self.__x = value
 
     @property
     def y(self):
         """Get the y-coordinate of the rectangle."""
-        return self._y
+        return self.__y
 
     @y.setter
     def y(self, value):
         """Set the y-coordinate of the rectangle."""
-        self._y = value
+        self.__y = value
 
 
