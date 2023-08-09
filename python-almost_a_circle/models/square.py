30c1c16778f1809fe27ef680e34724fe4307217c
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-09 14:01:46 +0100

    add square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+"""This module defines the Square class."""
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """This class represents a rectangle."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a square.
+
+        Args:
+            size: The size of the square.
+            x: The x-coordinate of the square.
+            y: The y-coordinate of the square.
+            id: The ID of the square.
+        """
+        super().__init__(size, size, x, y, id)
+        
+    
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+
+        Returns:
+            str: The formatted string containing the square information.
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
