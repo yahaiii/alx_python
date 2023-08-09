acdce31b7fd088f123648ddc2ae235e51fb34b19
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-09 04:24:46 +0100

    modify rectangle, add __str__

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -129,3 +129,13 @@ class Rectangle(Base):
         """
         for _ in range(self.__height):
             print("#" * self.__width)
+    
+    def __str__(self):
+        """
+        Overrides __str__
+
+        Returns:
+            formatted string of rectange attributes.
+        """
+        return("[Rectangle] ({}) {}/{} - {}/{}").format(self.id, self.__x, self.__y, self.__width, self.__height)
+
