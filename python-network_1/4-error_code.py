4d884e9141323532a55a6ea52ad6945fc7e1c20a
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-10 21:45:39 +0100

    modify whole of 4-error_code

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,10 +1,14 @@
 import requests
 import sys
 
-url = sys.argv[1]
-response = requests.get(url)
+def get_request(url):
+    """Sends a GET request to the given URL."""
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    return response.text
 
-if response.status_code >= 400:
-    print("Error code:", response.status_code)
-else:
-    print(response.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response_text = get_request(url)
+    print(response_text)
