e59de3dfc00d29d91e072254bdd83acc3e0ba88e
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-23 15:22:23 +0100

    add 4-cities_by_states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+"""lists all cities from the database"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+
+    query = "SELECT * FROM cities ORDER BY id ASC"
+
+    cur.execute(query)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
