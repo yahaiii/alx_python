5f2c46eebe79b6a6afcdc20be9e60dbdd64b443c
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-23 14:29:49 +0100

    add 2-my_filter_states

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+"""that takes in an argument and displays all values in states table where name matches the argument"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    arg = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE '{}' COLLATE ORDER BY id ASC".format(arg)
+
+    cur.execute(query)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
