e3e72c9a0bf27a1970af11d313c9cc97ef92b9c6
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-23 13:51:55 +0100

    modify 0-select, fix whitespaces

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost", 
-                         port=3306, 
-                         user=username, 
-                         passwd=password, 
-                         db=database)
+                        port=3306, 
+                        user=username, 
+                        passwd=password, 
+                        db=database)
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY id ASC")
