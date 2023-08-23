a790fa137a330aec3ab13284ff5e70a111d1c579
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-23 13:46:22 +0100

    initial commit

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+"""A script that lists all states from the database"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+        
+    cur.close()
+    db.close()
