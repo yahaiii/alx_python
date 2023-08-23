36f6e9444d314cb2453912223a5c7023ff800596
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-23 15:13:08 +0100

    add 3-my_safe_filter_states

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+""" a safe version of my_filter_states """
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+
+    query = """
+    SELECT *
+    FROM states
+    WHERE name
+    LIKE BINARY %s
+    ORDER BY id ASC
+    """
+
+    cur.execute(query, state)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
