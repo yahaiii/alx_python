281ad488b214ffed638c6d2ff47e08cb735eedf1
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-23 13:53:28 +0100

    remove trailing whitespace

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,11 +9,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", 
-                        port=3306, 
-                        user=username, 
-                        passwd=password, 
-                        db=database)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY id ASC")
