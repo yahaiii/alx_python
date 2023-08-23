c56dd1e5bfb9e81a81e7478436aabba3806bdb47
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-23 14:32:05 +0100

    modify 2-my_filter_states, remove COLLATE from query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = "SELECT * FROM states WHERE name LIKE '{}' COLLATE ORDER BY id ASC".format(arg)
+    query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(arg)
 
     cur.execute(query)
 
