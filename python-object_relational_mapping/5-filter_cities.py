e74f43fe8c3535f64bc6e493c0d9b4b04b3081ba
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-23 15:50:46 +0100

    add 5-filter_cities

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+"""takes in the name of a state as an argument and lists all cities"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+
+    query = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY id ASC
+    """
+
+    cur.execute(query, (state,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
