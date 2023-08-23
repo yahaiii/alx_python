6db913c936af6b19fdcc9ec56649e3436cfaf2a7
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-23 15:53:26 +0100

    modify 5-filter, update query

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     query = """
-    SELECT cities.id, cities.name, states.name
+    SELECT cities.name
     FROM cities
     JOIN states ON cities.state_id = states.id
     WHERE states.name = %s
