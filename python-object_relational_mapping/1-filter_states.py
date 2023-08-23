266fc2958ba66780f961115150e6b21779ed04b4
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-23 14:55:56 +0100

    modify 1-filter_states, LIKE matter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,13 +19,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = """
-    SELECT * 
-    FROM states 
-    WHERE name 
-    LIKE BINARY 'N%'
-    ORDER BY id ASC"
-    """
+    query = "SELECT * FROM states WHERE UPPER(name) LIKE 'N%' ORDER BY id ASC"
 
     cur.execute(query)
 
