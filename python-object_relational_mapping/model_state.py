2797a8b90992a358520b24fec1fd3b847c551187
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-24 13:08:55 +0100

    modify model_state, remove module comment

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,27 +1,20 @@
-"""
-Represents a state entity in the database.
-"""
-
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    # Create a base class for declarative models
-    Base = declarative_base()
+Base = declarative_base()
 
-    #create class State
-    class State(Base):
+#create class State
+class State(Base):
+    """
+     This class inherits from SQLAlchemy's declarative base class and maps to the 'states' table.
+
+    Attributes:
+        id (int): The unique identifier for the state.
+        name (str): The name of the state.
+
+    Args:
+        Base (DeclarativeMeta): The base class for declarative models from SQLAlchemy.
         """
-        This class inherits from SQLAlchemy's declarative base class and maps to the 'states' table.
-
-        Attributes:
-            id (int): The unique identifier for the state.
-            name (str): The name of the state.
-
-        Args:
-            Base (DeclarativeMeta): The base class for declarative models from SQLAlchemy.
-        """
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True, autoincrement=True)
-        name = Column(String(128), nullable=False)
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
