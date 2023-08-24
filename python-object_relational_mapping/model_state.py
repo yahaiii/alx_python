e64ca1b1ffa6b6cd8860801d1664990400a1b01a
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-24 12:56:24 +0100

    modify model_state, update class docstring

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,26 +1,27 @@
-"""
-Represents a State table in the database.
-"""
-
-
 import sys
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 # Create a database engine using SQLAlchemy's create_engine function
 # It connects to a MySQL database using the provided username, password, and database name
-
-# Create a base class for declarative models
-Base = declarative_base()
-
-class State(Base):
-    """
-    Represents a State table in the database.
-    
-    It defines columns for 'id' and 'name'.
-    """
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(128), nullable=False)
-
 if __name__ == "__main__":
+    # Create a base class for declarative models
+    Base = declarative_base()
+
+    class State(Base):
+        """
+        Represents a state entity in the database.
+
+        This class inherits from SQLAlchemy's declarative base class and maps to the 'states' table.
+        It defines the structure of the 'states' table with columns for 'id' and 'name'.
+
+        Attributes:
+            id (int): The unique identifier for the state (auto-incremented primary key).
+            name (str): The name of the state, limited to 128 characters, and cannot be null.
+
+        Args:
+            Base (DeclarativeMeta): The base class for declarative models from SQLAlchemy.
+        """
+        __tablename__ = 'states'
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        name = Column(String(128), nullable=False)
