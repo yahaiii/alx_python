e05a14bafb8dac94d53f9b2bd695152498c8d301
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-24 13:56:36 +0100

    modify 9-model, satisfy pep8

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     # fetch states that contain letter a
     states = session.query(State)\
         .filter(State.name.like('%a%'))\
-            .order_by(State.id)\
-                .all()
+        .order_by(State.id)\
+        .all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
