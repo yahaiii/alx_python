6c973fb4333638d6ed0ceda2f50a7bdb9f41883b
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-24 12:06:24 +0100

    modify model_state, add docstrings

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,32 @@
+"""
+Represents a State table in the database.
+"""
+
+
 import sys
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+# Create a database engine using SQLAlchemy's create_engine function
+# It connects to a MySQL database using the provided username, password, and database name
 engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
+# Create a base class for declarative models
 Base = declarative_base()
 
 class State(Base):
+    """
+    Represents a State table in the database.
+    
+    This class inherits from SQLAlchemy's declarative base class and maps to the 'states' table.
+    It defines columns for 'id' and 'name'.
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
-    Base.metadata.create_all()
+    """
+    This block creates the database tables when the script is run directly.
+    """
+    Base.metadata.create_all(engine)
