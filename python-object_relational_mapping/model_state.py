7bc17ada5acf9f15630258b5ac903ebbb6d32f9b
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-24 13:05:13 +0100

    modify model_state, add comment above class

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,13 +10,14 @@ if __name__ == "__main__":
     # Create a base class for declarative models
     Base = declarative_base()
 
+    #create class State
     class State(Base):
         """
         This class inherits from SQLAlchemy's declarative base class and maps to the 'states' table.
 
         Attributes:
-            id (int): The unique identifier for the state (auto-incremented primary key).
-            name (str): The name of the state, limited to 128 characters, and cannot be null.
+            id (int): The unique identifier for the state.
+            name (str): The name of the state.
 
         Args:
             Base (DeclarativeMeta): The base class for declarative models from SQLAlchemy.
