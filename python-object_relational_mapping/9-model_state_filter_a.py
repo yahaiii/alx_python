ec0cdd7bfaaccda97058ffa9856965c31e68f28c
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-24 13:55:25 +0100

    modify 9-model, make query multiline

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,14 +6,17 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
- 
+
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # fetch states that contain letter a
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State)\
+        .filter(State.name.like('%a%'))\
+            .order_by(State.id)\
+                .all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
