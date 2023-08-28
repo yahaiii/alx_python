03f1ae824ba0273f5d1b1655eb9b6e31b4a45837
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-28 14:46:22 +0100

    add 1-hbnb_route

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,34 @@
+"""
+This is a simple Flask web application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    Route handler for the root URL "/". Displays a greeting message.
+
+    Returns:
+    str: A string containing the greeting message "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Route handler for the root URL "/hbnb". Displays a string.
+
+    Returns:
+    str: A string containing the greeting message "HBNB".
+    """
+    return "HBNB"
+
+if __name__ == "__main__":
+    """
+    Start the Flask web application.
+    """
+    app.run(host="0.0.0.0", port=5000)
