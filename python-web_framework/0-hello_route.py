da0a4a2fa2032dcc86edfb31f7fd3a3d17a09538
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-08-28 14:40:26 +0100

    modify 0-hello, add docstrings

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,10 +1,29 @@
+"""
+This is a simple Flask web application that implements routing using the Flask web framework.
+
+Usage:
+- Start the application by running this script.
+- Open your web browser and navigate to http://127.0.0.1:5000/ to see the greeting message. 
+- Navigate to different paths to see the result displayed in the web browser.
+
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """
+    Route handler for the root URL "/". Displays a greeting message.
+
+    Returns:
+    str: A string containing the greeting message "Hello HBNB!".
+    """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
+    """
+    Start the Flask web application.
+    """
     app.run(host="0.0.0.0", port=5000)
