516629f42722e866b5f71e93e01e82b98df31424
yahaiii <wanekaxa@gmail.com>
2023-10-05 12:26:06 +0100

    add 1-export_to_csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,43 @@
+import csv
+import requests
+import sys
+
+def get_employee_todo_progress(employee_id):
+    # Fetch employee details
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    employee_response = requests.get(employee_url)
+    
+    if employee_response.status_code != 200:
+        print(f"Employee with ID {employee_id} not found.")
+        return
+    
+    employee_data = employee_response.json()
+    employee_name = employee_data['name']
+
+    # Fetch employee's TODO list
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    todos_response = requests.get(todos_url)
+    
+    if todos_response.status_code != 200:
+        print(f"Unable to fetch TODO list for employee with ID {employee_id}.")
+        return
+    
+    todos_data = todos_response.json()
+
+    # Create CSV file and write data
+    csv_filename = f"{employee_id}.csv"
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        
+        # Write task data
+        for todo in todos_data:
+            writer.writerow([
+                "'{}'".format(employee_id),
+                "'{}'".format(employee_name),
+                "'{}'".format(str(todo["completed"])),
+                "'{}'".format(todo["title"])
+            ])
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
