0367a3a47467f44106c0ebe10d7a786d259742f7
yahaiii <wanekaxa@gmail.com>
2023-10-05 11:57:49 +0100

    modify 0-gather_data, fix output format

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -36,10 +36,6 @@ def get_employee_todo_progress(employee_id):
     return progress
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
     employee_id = int(sys.argv[1])
     progress = get_employee_todo_progress(employee_id)
     
