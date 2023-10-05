e2d20a4ab51708a7db952d6e8a3555267a02b490
yahaiii <wanekaxa@gmail.com>
2023-10-05 14:10:40 +0100

    modify 1-export, triple quotes issue

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,6 +3,12 @@ import requests
 import sys
 
 def get_employee_todo_progress(employee_id):
+    """
+    Fetches an employee's TODO list, creates a CSV file, and writes the data.
+    
+    Args:
+        employee_id (int): The ID of the employee for whom to fetch TODO list and create a CSV file.
+    """
     # Fetch employee details
     employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     employee_response = requests.get(employee_url)
