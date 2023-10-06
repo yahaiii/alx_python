c833b5e7c7b7586f6c85ed826eac51b54fc3556f
yahaiii <wanekaxa@gmail.com>
2023-10-06 10:39:00 +0100

    modify 1-export, use QUOTE_ALL

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,59 +1,4 @@
-# import csv
-# import requests
-# import sys
-
-# def export_employee_todo_progress_to_csv(employee_id):
-#     """
-#     Fetches an employee's TODO list, creates a CSV file, and writes the data.
-
-#     Args:
-#         employee_id (int): The ID of the employee for whom to fetch TODO list and create a CSV file.
-#     """
-#     # Fetch employee details
-#     employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-#     employee_response = requests.get(employee_url)
-
-#     if employee_response.status_code != 200:
-#         print(f"Employee with ID {employee_id} not found.")
-#         return
-
-#     employee_data = employee_response.json()
-#     employee_username = employee_data['username']
-
-#     # Fetch employee's TODO list
-#     todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-#     todos_response = requests.get(todos_url)
-
-#     if todos_response.status_code != 200:
-#         print(f"Unable to fetch TODO list for employee with ID {employee_id}.")
-#         return
-
-#     todos_data = todos_response.json()
-
-#     # Create CSV file and write data
-#     csv_filename = f"{employee_id}.csv"
-#     with open(csv_filename, mode='w', newline='') as csv_file:
-#         writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL, delimiter=',', quotechar='"', escapechar='\\')
-
-#         # Write CSV header
-#         writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-#         # Write task data
-#         # without quotes
-#         # for todo in todos_data:
-#         #     csv_line = [employee_id, employee_username, str(todo["completed"]), todo["title"]]
-#         #     writer.writerow(csv_line)
-
-#         # With double quotes
-#         for todo in todos_data:
-#             csv_line = [employee_id, employee_username, str(todo["completed"]), todo["title"]]
-#             writer.writerow(csv_line)
-
-# if __name__ == "__main__":
-#     employee_id = int(sys.argv[1])
-#     export_employee_todo_progress_to_csv(employee_id)
-
-
+import csv
 import requests
 import sys
 
@@ -88,14 +33,69 @@ def export_employee_todo_progress_to_csv(employee_id):
     # Create CSV file and write data
     csv_filename = f"{employee_id}.csv"
     with open(csv_filename, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL, delimiter=',', quotechar='"', escapechar='\\')
 
-        # Write task data with values enclosed in double quotes
-        for index, todo in enumerate(todos_data):
-            csv_line = f'"{employee_id}","{employee_username}","{str(todo["completed"])}","{todo["title"]}"'
-            if index < len(todos_data) - 1:  # Check if it's not the last iteration
-                csv_line += '\n'
-            csv_file.write(csv_line)
+        # Write CSV header
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        # Write task data
+        # without quotes
+        # for todo in todos_data:
+        #     csv_line = [employee_id, employee_username, str(todo["completed"]), todo["title"]]
+        #     writer.writerow(csv_line)
+
+        # With double quotes
+        for todo in todos_data:
+            csv_line = [employee_id, employee_username, str(todo["completed"]), todo["title"]]
+            writer.writerow(csv_line)
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     export_employee_todo_progress_to_csv(employee_id)
+
+
+# import requests
+# import sys
+
+# def export_employee_todo_progress_to_csv(employee_id):
+#     """
+#     Fetches an employee's TODO list, creates a CSV file, and writes the data.
+
+#     Args:
+#         employee_id (int): The ID of the employee for whom to fetch TODO list and create a CSV file.
+#     """
+#     # Fetch employee details
+#     employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+#     employee_response = requests.get(employee_url)
+
+#     if employee_response.status_code != 200:
+#         print(f"Employee with ID {employee_id} not found.")
+#         return
+
+#     employee_data = employee_response.json()
+#     employee_username = employee_data['username']
+
+#     # Fetch employee's TODO list
+#     todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+#     todos_response = requests.get(todos_url)
+
+#     if todos_response.status_code != 200:
+#         print(f"Unable to fetch TODO list for employee with ID {employee_id}.")
+#         return
+
+#     todos_data = todos_response.json()
+
+#     # Create CSV file and write data
+#     csv_filename = f"{employee_id}.csv"
+#     with open(csv_filename, mode='w', newline='') as csv_file:
+
+#         # Write task data with values enclosed in double quotes
+#         for index, todo in enumerate(todos_data):
+#             csv_line = f'"{employee_id}","{employee_username}","{str(todo["completed"])}","{todo["title"]}"'
+#             if index < len(todos_data) - 1:  # removes blank line at EoF
+#                 csv_line += '\n'
+#             csv_file.write(csv_line)
+
+# if __name__ == "__main__":
+#     employee_id = int(sys.argv[1])
+#     export_employee_todo_progress_to_csv(employee_id)
